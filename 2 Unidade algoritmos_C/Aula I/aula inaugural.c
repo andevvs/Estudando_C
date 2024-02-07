ce72ca8e9139a6b401e52e95924c7ea86bd15ff3
anDev <andreiviesil@gmail.com>
2024-02-07 14:04:02 -0300

    ...

---
+++ 2 Unidade algoritmos_C/Aula I/aula inaugural.c
@@ -0,0 +1,6 @@
+//escreva um programa utilizando C para mostrar na tela a seguinte mensagem
+//"C é a melhor linguagem"
+#include <stdio.h>
+int main (){
+    printf("C\n é a melhor\n linguagem");
+}
--- 2 Unidade algoritmos_C/Aula I/main.c
+++
@@ -1,6 +0,0 @@
-//escreva um programa utilizando C para mostrar na tela a seguinte mensagem
-//"C é a melhor linguagem"
-#include <stdio.h>
-int main (){
-    printf("C\n é a melhor\n linguagem");
-}
