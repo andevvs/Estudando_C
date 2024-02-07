0aa41b91984b67e3ddcd238d5b8cd6b3b742ccde
anDev <andreiviesil@gmail.com>
2024-02-06 21:01:09 -0300

    exercicios solicitados em aula

---
+++ 2 Unidade algoritmos_C/Aula I/exercicios/1.informar_x.c
@@ -0,0 +1,10 @@
+//faça um programa que informe o valor de x
+#include <stdio.h>
+
+int main()
+{
+int x;
+ printf("informe o valor de x");
+ scanf("%d", &x);
+ printf("o valor de x é %d", x);
+}
---
+++ 2 Unidade algoritmos_C/Aula I/exercicios/2.soma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+int n1, n2, r;
+ printf("informe o valor dos dois numeros:");
+ scanf("%d %d", &n1, &n2);
+ r = n1+n2;
+ printf("o resultado é %d", r);
+}
