16c4b5752c748654c31029cebb4d1237c83963b8
anDev <andreiviesil@gmail.com>
2024-03-20 14:52:12 -0300

    fix

---
+++ III_Unidade_algoritmos_C/funções/exemplo_função_SemRetorno.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Declaração da função 
+void verifica(int p){
+    if(p == 0){
+        printf("Igual a zero");
+    }
+    else{
+        printf("Diferente de zero");
+    }
+}
+
+int main(){
+    int p;
+    printf("Digite um valor inteiro:");
+    scanf("%d", &p);
+    // Chamada da função 
+    verifica(p);
+    return(0);
+}
