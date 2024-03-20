e6128ef80fc353637000ce5f3808d0b4e02e4e04
anDev <andreiviesil@gmail.com>
2024-03-20 20:59:45 -0300

    sla

---
+++ III_Unidade_algoritmos_C/funções/exemplo_função_ComRetorno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Esta função calcula o quadrado de um número inteiro e retorna o resultado.
+int calcularQuadrado(int num) {
+//int quadrado = num * num;
+    return  num * num;
+}
+
+int main() {
+    int numero, resultado, bosta;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+// Chama a função calcularQuadrado() e armazena o resultado retornado na variável 'resultado'.
+    resultado = calcularQuadrado(numero);
+    printf("O quadrado de %d e %d.\n", numero, resultado);
+    return 0;
+}
