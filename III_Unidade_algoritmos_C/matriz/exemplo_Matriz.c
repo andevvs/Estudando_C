7219de68b4b3a7f353e5b5e23a832eef2d3a05e7
anDev <andreiviesil@gmail.com>
2024-03-21 11:50:16 -0300

    create

--- III_Unidade_algoritmos_C/matriz/exemplo_Matriz.c
+++ III_Unidade_algoritmos_C/matriz/exemplo_Matriz.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    int matriz [3][3] = {{1,2,3},
+                         {4,5,6},
+                         {7,8,9}};
+    int l, c;
+//Navegando pelos indices e colunas de uma matriz com laço for
+for (l = 0; l < 3; l++){
+    for (c = 0; c< 3; c++){
+        printf(" %d ", matriz[l][c]);
+    } 
+    printf("\n");
+}
+
     return(0);
 }
