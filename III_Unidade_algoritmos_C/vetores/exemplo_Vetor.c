1e9c333dc4e93119e80dd3ae5c3aa5ebffaa7a5f
anDev <andreiviesil@gmail.com>
2024-03-21 11:27:13 -0300

    update

--- III_Unidade_algoritmos_C/vetores/exemplo_Vetor.c
+++ III_Unidade_algoritmos_C/vetores/exemplo_Vetor.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int vetor [10] = {1,2,3,4,5,6,7,8,9,10};
+    int vetor [5] = {1,2,3,4,5};
     printf("Conteudo da Primeira posicao: %d. \n", vetor [0]);
-
+    printf("Conteudo de todo o vetor: %d %d %d %d %d",vetor [0], vetor [1], vetor [2], vetor [3], vetor [4] );
     return(0);
 }
