ddde110a5ec66fc65c9fb6e36623bb07d5c462a4
anDev <andreiviesil@gmail.com>
2024-04-13 11:35:48 -0300

    feat: create ate zero

---
+++ III_Unidade_algoritmos_C/exercicios/12.AteZero.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int vet[20];
+    int tamanho= 0;
+    int valor;
+    int i;
+    printf("Digite os Elementos do vetor (0 para parar):\n");
+    do{
+    scanf("%d", &valor);
+    if (valor != 0){
+        vet[tamanho]= valor;
+        tamanho++;
+    }
+  } while (valor!=0);
+     printf("Digite um numero para procurar no vetor: ");
+    scanf("%d", &valor);
+ for (i = 0; i < tamanho; i++) {
+        if (vet[i] == valor) {
+            printf("O numero %d esta presente na posicao %d do vetor.\n", valor, i);
+            break;
+        }
+    }
+        if (i == tamanho) {
+        printf("O namero %d não esta presente no vetor.\n", valor);
+    }
+
+    return 0;
+}
