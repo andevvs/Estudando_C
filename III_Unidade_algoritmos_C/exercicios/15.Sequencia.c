2243f24f6c75fdd959ea10535a044db93c30cdac
anDev <andreiviesil@gmail.com>
2024-04-15 11:08:53 -0300

    feat: create sequencia.c

---
+++ III_Unidade_algoritmos_C/exercicios/15.Sequencia.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    printf("Digite o numero de elementos da sequencia: ");
+    scanf("%d", &n);
+
+    float sequencia[n];
+    int frequencia[n];
+    int cont = 0;
+    printf("Digite os elementos da sequencia:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%f", &sequencia[i]);
+        frequencia[i] = -1; 
+    }
+
+    for (int i = 0; i < n; i++) {
+        int freq = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (sequencia[i] == sequencia[j]) {
+                freq++;
+                frequencia[j] = 0; 
+            }
+        }
+        if (frequencia[i] != 0) {
+            frequencia[i] = freq;
+        }
+    }
+    
+    printf("Saida:\n");
+    for (int i = 0; i < n; i++) {
+        if (frequencia[i] != 0) {
+            printf("%.2f ocorre %d vez(es)\n", sequencia[i], frequencia[i]);
+        }
+    }
+    return 0;
+}
