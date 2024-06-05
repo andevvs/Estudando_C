e08a30154b7344a420ca411478e92d221a1800b6
anDev <andreiviesil@gmail.com>
2024-06-05 14:24:46 -0300

    feat: create exemplo_alocacao

---
+++ Estrutura_de_Dados_I/Alocacao_dinamica/exemplo_alocacao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* A alocação dinâmica é feita através da função malloc ou calloc. 
+
+int *x = malloc(5 * sizeof(int));
+
+ou, com a função calloc
+
+int *x = calloc(5, sizeof(int));
+
+A diferenciação entre as duas funções é que a função calloc inicializa cada posição do vetor com zeros.
+*/
+
