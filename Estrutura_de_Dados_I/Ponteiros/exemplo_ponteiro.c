988e2e5648f5cf1ff3a1455eb176072d4ff46905
anDev <andreiviesil@gmail.com>
2024-06-05 14:16:12 -0300

    feat: create exemplo_ponteiro

---
+++ Estrutura_de_Dados_I/Ponteiros/exemplo_ponteiro.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/* Para declarar um ponteiro, basta colocar um asterisco antes do nome da variável.
+ int * p;*/
+
+/* Para atribuir um ponteiro, basta colocar o endereço da variável que ele apontará.
+ int x=10
+ int * p= &x */
+
+ /* Para acessar o valor de um ponteiro, basta colocar um asterisco antes do nome da variável.
+ 
+int x = 10;
+int *p = &x;
+
+printf("%d", *p); */
+
+int main(void){
+    int x = 20;
+    int *p = &x;
+
+    printf("%d", *p);
+}
