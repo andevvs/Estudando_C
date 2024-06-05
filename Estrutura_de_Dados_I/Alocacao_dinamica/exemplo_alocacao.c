1ac66551e62b88e95eb349b592adcc82eb92f71f
anDev <andreiviesil@gmail.com>
2024-06-05 14:36:19 -0300

    feat: create exemplo

--- Estrutura_de_Dados_I/Alocacao_dinamica/exemplo_alocacao.c
+++ Estrutura_de_Dados_I/Alocacao_dinamica/exemplo_alocacao.c
@@ -11,4 +11,23 @@ int *x = calloc(5, sizeof(int));
 
 A diferenciação entre as duas funções é que a função calloc inicializa cada posição do vetor com zeros.
 */
+void ler_vetor(float * vetor, int tamanho){
+    //funcao para ler valores de um vetor de numeros reais
+    printf("Informe os valores para o vetor: \t");
+      for (int index; index<tamanho; index++){
+        scanf("&f", &vetor[index]);
+    }
+}
 
+int main(void){
+    
+    int tamanho = 5;
+    float * vetor = (float*) malloc (tamanho*sizeof(float));
+    if(vetor==NULL){
+        printf("Sem memoria\n");
+        exit(1);
+    }
+    else{
+        printf("Vetor Alocado!\n");
+    }
+}
