8f1b89162871e13df289c5f5c779e0852dc3b842
anDev <andreiviesil@gmail.com>
2024-07-10 09:08:30 -0300

    feat: create question 2

---
+++ Estrutura_de_Dados_I/Exercicios/2_matriz.c
@@ -0,0 +1,10 @@
+/*2-Implemente um programa em C que solicite ao usu´ario o n´umero de linhas e colunas de uma matriz e,
+em seguida, aloque dinamicamente mem´oria para armazenar essa matriz. Pe¸ca ao usu´ario que insira
+os elementos da matriz e, por fim, imprima a matriz na forma original e transposta.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    
+}
