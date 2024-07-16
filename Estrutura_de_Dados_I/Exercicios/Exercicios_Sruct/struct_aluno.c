664365acbbdfb0f4573957be0d3f2f1ac1dd2d59
anDev <andreiviesil@gmail.com>
2024-07-16 16:55:44 -0300

    fix

--- Estrutura_de_Dados_I/Exercicios/Exercicios_Sruct/struct_aluno.c
+++ Estrutura_de_Dados_I/Exercicios/Exercicios_Sruct/struct_aluno.c
@@ -1,12 +1,23 @@
-struct aluno{
-    int mat;
-    int idade;
+#include <stdio.h>
+typedef struct curso {
     char nome[20];
-};
+    int cod;
+} Curso;
 
-int main(){
-    struct aluno a;
-    captura(&a);
-    imprime(&a);
+typedef struct aluno {
+    int mat, idade;
+    char nome[20];
+    Curso c;
+} Aluno;
+
+int main (void){
+    Aluno a;
+    printf("Digite o nome:");
+    scanf("%[^\n]", a.nome);
+    printf("Informe o Curso:");
+    scanf("%[^\n]", a.c.nome);
+    printf("informe o codigo do curso:");
+    scanf("%d", &a.c.cod);
+
     return 0;
 }
