8cac6c8daba7a02016800de98f873a2967aec2dc
anDev <andreiviesil@gmail.com>
2024-07-22 22:53:31 -0300

    fix: fixxed bugs

--- Estrutura_de_Dados_I/1º_Unidade/Struct/Exercicios_Struct/FeitosEmSala/struct_pontos.c
+++ Estrutura_de_Dados_I/1º_Unidade/Struct/Exercicios_Struct/FeitosEmSala/struct_pontos.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 #include <math.h>
-
-struct circulo{
-    Ponto p;/*centro do circulo*/
-    float r;/*raio*/
-};
-typedef struct circulo Circulo;
-
-int interior (Circulo* c, Ponto* p){
-    float d = distancia(&c->p, p);
-    return(d <c->r);
-}
-
-struct ponto {
+typedef struct ponto {
     float x;
     float y;
-};
-typedef struct ponto Ponto;
+}Ponto;
 
-float distancia (Ponto* p, Ponto* q){
-    float d = sqrt ((q->x - p->x)* (q->x - p->x)
-                + (q->y - p->y) * (q->y - p->y));
+float distancia(Ponto* p, Ponto* q) {
+    float d = sqrt((q->x - p->x) * (q->x - p->x) + (q->y - p->y) * (q->y - p->y));
     return d;
 }
 
-int main(){
-    float p, q;
-    printf("Informe o ponto x:");
+typedef struct circulo {
+    Ponto p; /* centro do circulo */
+    float r; /* raio */
+}Circulo;
+
+
+int interior(Circulo* c, Ponto* p) {
+    float d = distancia(&c->p, p);
+    return (d < c->r);
+}
+
+int main() {
+    Ponto p;
+    Ponto q;
+    Circulo c;
+    printf("Informe o ponto x do centro do círculo: ");
+    scanf("%f", &c.p.x);
+    printf("Informe o ponto y do centro do círculo: ");
+    scanf("%f", &c.p.y);
+    printf("Informe o raio do círculo: ");
+    scanf("%f", &c.r);
+    printf("Informe o ponto x a ser testado: ");
     scanf("%f", &p.x);
-    printf("Informe o ponto y:");
-    scanf("%f", &q.y);
-    distancia(p,q);
+    printf("Informe o ponto y a ser testado: ");
+    scanf("%f", &p.y);
+    if (interior(&c, &p)) {
+        printf("O ponto está dentro do círculo.\n");
+    } else {
+        printf("O ponto está fora do círculo.\n");
+    }
+
+    return 0;
 }
