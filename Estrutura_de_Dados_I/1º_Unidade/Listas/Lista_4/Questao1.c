53d0b01eb863381dccc0c7591f2117072945e965
anDev <andreiviesil@gmail.com>
2024-07-29 13:34:08 -0300

    fix: alterações no codigo

--- Estrutura_de_Dados_I/1º_Unidade/Listas/Lista_4/Questao1.c
+++ Estrutura_de_Dados_I/1º_Unidade/Listas/Lista_4/Questao1.c
@@ -13,23 +13,23 @@ quadrado ocupado por terra que tem um lado em comum com um quadrado ocupado por
 
 int main() {
     int i = 0;
-    int M, N;
+    int L, C;
     char mapa[MAXIMO][MAXIMO];
     printf("Informe o tamanho do mapa: ");
     // le o tamanho do mapa
-    scanf(" %d %d", &M, &N);
+    scanf(" %d %d", &L, &C);
     // le o mapa
-    for (i=0; i < M; i++) {
+    for (i=0; i < L; i++) {
         scanf(" %[^\n]", mapa[i]);
     }
     int costa = 0;
     // percorre o mapa para encontrar os quadrados que sao parte da costa
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < C; j++) {
             if (mapa[i][j] == '#') {
                 // vai verificar se o quadrado atual tem algum vizinho que é água ou está fora dos limites
-                if (i == 0 || mapa[i-1][j] == '.' || i == M-1 || mapa[i+1][j] == '.' ||
-                    j == 0 || mapa[i][j-1] == '.' || j == N-1 || mapa[i][j+1] == '.') {
+                if (i == 0 || mapa[i-1][j] == '.' || i == L-1 || mapa[i+1][j] == '.' ||
+                    j == 0 || mapa[i][j-1] == '.' || j == L-1 || mapa[i][j+1] == '.') {
                     costa++;
                 }
             }
