ff3c36e20c335e46fa837de516aafb9aa83d9c1c
anDev <andreiviesil@gmail.com>
2024-07-29 15:19:45 -0300

    fix: mudanças no aluno.h

--- Estrutura_de_Dados_I/2º_Unidade/ManipulacaoDeArquivos/exemplo_manipulacaoI.c/aluno.h
+++ Estrutura_de_Dados_I/2º_Unidade/ManipulacaoDeArquivos/exemplo_manipulacaoI.c/aluno.h
@@ -1,10 +1,13 @@
 //Arquivo Cabeçalho para tipo abstrato de dados (TAD) Aluno
 
+//Tipo exportado
 typedef struct aluno Aluno;
+
+/*Função que aloca dinamicamente uma struct aluno na memoria*/
+Aluno * aloca_aluno(void);
 
 /*Função para preencher uma struct Aluno. 
 A função recebe um endereço para aluno*/
-
 void preenche(Aluno * aluno);
 
 /*Função para imprimir uma struct Aluno*/
