fc336fdffe523115dbbefa8bb4deb8f2bf535cc4
anDev <andreiviesil@gmail.com>
2024-09-02 11:51:50 -0300

    style: formatação

--- Estrutura_de_Dados_I/2º_Unidade/TAD/Exercicios_Tad/Funcionarios/Funcionario.h
+++ Estrutura_de_Dados_I/2º_Unidade/TAD/Exercicios_Tad/Funcionarios/Funcionario.h
@@ -1,6 +1,3 @@
-#ifndef FUNCIONARIO_H
-#define FUNCIONARIO_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,12 +7,17 @@
 //definindo a struct Funcionario
 typedef struct funcionario Funcionario;
 
-//funções
+//função par alocar dinamicamente um funcionario
 Funcionario **aloca_funcionarios();
+//função para liberar a memoria alocada
 void libera_funcionario(Funcionario **funcionarios, int qnt);
+//função para buscar um funcionario
 Funcionario *busca_funcionario(Funcionario **funcionarios, int id, int qnt);
+//função para adicionar um funcionario
 Funcionario **adiciona_funcionario(Funcionario **funcionarios, char *nome, int id, float salario, int *qnt);
+//função para remover um funcionario
 Funcionario **remover_funcionario(Funcionario **funcionarios, int id, int *qnt);
+//função para listar os funcionarios adicionados
 void listar_funcionarios(Funcionario **funcionarios, int qnt);
 
-#endif 
+
