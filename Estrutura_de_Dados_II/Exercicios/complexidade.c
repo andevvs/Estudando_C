36c5a385f1075238df39c8201da94a4fe31e20f6
anDev <andreiviesil@gmail.com>
2025-05-15 14:50:12 -0300

    fix: correção no algoritmo

--- Estrutura_de_Dados_II/Exercicios/complexidade.c
+++ Estrutura_de_Dados_II/Exercicios/complexidade.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <time.h>
-#include <math.h>
+#include <stdlib.h>
 
-//funcao fibonacci
-int fibonacci(int n){
-    if(n <=1){
+int fibonaccil(int  n){
+    if(n <=  1){
         return n;
     }
-    return fibonacci(n - 1) + fibonacci(n - 1);
+    return fibonaccil(n - 1) + fibonaccil(n - 2);
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
     int n = atoi(argv[1]);
     int n_max = atoi(argv[2]);
     clock_t start, end;
     double tempo = 0.0;
     int resultado;
 
-    for (size_t i = 0; i< n_max; i++) {
+    for(size_t i = 0; i < n_max;i++){
         start = clock();
-        resultado = fibonacci(n);
+        resultado = fibonaccil(n);
         end = clock();
         tempo += ((double)(end - start)) / CLOCKS_PER_SEC;
-        printf("Progresso: %.2f%% concluido\n", ((i+1) / (double)n_max) * 100);
+        printf("Progresso: %.2f%% Concluido\n", ((i + 1) / (double)n_max) * 100);
     }
 
-    printf("Fibonacci na posicao %d is %d\n", n, resultado);
-    printf("Tempo medio: %f is\n", (tempo / n_max) * 1e6);
-    return 0; 
+    printf("Fibonaccil na posicao %d is %d\n", n, resultado);
+    printf("Tempo medio: %f µs\n",(tempo / n_max) * 1e6);
 }
